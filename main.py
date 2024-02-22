93b51f978282012fac07b9ebcdc2789bde8e00c8
Marko2155 <98662057+Marko2155@users.noreply.github.com>
2024-02-22 16:07:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import urllib.request
+import requests as req
 import subprocess
 
 def install(package):
@@ -8,22 +8,26 @@ def install(package):
 
 def create_run_launcher():
     if os.path.exists("./assets/launcher.py"):
+        print("Reinstalling launcher.py")
         os.remove("./assets/launcher.py")
-    with urllib.request.urlopen("https://raw.githubusercontent.com/Marko2155/PyCrackedMC/main/assets/launcher.py") as f:
-        launcherCode = f.read().decode("utf-8")
-        with open("./assets/launcher.py", "w") as launcherFile:
-            launcherFile.write(launcherCode)
-            launcherFile.close()
-            subprocess.run([sys.executable, "./assets/launcher.py"])
+    url = "https://raw.githubusercontent.com/Marko2155/PyCrackedMC/main/assets/launcher.py"
+    res = req.get(url)
+    launcherCode = res.text
+    with open("./assets/launcher.py", "x") as f:
+        f.write(launcherCode)
+        f.close()
+    subprocess.run([sys.executable, "./assets/launcher.py"])
 
 try:
     import textual
 except ImportError:
+    print("[INFO] Could not find library 'textual' on system, installing")
     install("textual")
 
 try:
     import minecraft_launcher_lib
 except ImportError:
+    print("[INFO] Could not find library 'minecraft_launcher_lib' on system, installing")
     install("minecraft_launcher_lib")
 
 if os.path.exists("./assets"):
