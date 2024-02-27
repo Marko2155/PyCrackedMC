7c70f7bb28a98d38e539ebc69f7b5d186d7bb807
Marko2155 <98662057+Marko2155@users.noreply.github.com>
2024-02-27 10:22:28 +0100

    Update launcher.py

--- assets/launcher.py
+++ assets/launcher.py
@@ -1,97 +1,52 @@
-import subprocess
-
-from textual.app import *
-from textual.widgets import *
-from textual.containers import *
-from textual.screen import *
+import sys
+import tkinter as tk
 from minecraft_launcher_lib import *
-import webbrowser
 import random
 import subprocess
 import os
+import sys
 
-path = os.getcwd()
+# New Tkinter Launcher
 
-minecraft_directory = utils.get_minecraft_directory()
 minecraft_version = utils.get_latest_version()["release"]
-
+minecraft_directory = utils.get_minecraft_directory()
+path = os.getcwd()
 options = {
     "username": "PCMPlayer" + str(random.randrange(0, 10000)),
-    "uuid": "30c807a0-7d6d-44cf-960b-a7e8c1bafc7c",
+    "uuid": "a96235c5-9da2-436f-a45d-c38dc84ad28f",
     "token": "pineapple-pizza"
 }
 
-
-class LoginPanel(Screen):
-
-    def on_button_pressed(self, event: Button.Pressed):
-        if event.button.id == "settings":
-            app.pop_screen()
-            app.push_screen("settings")
-        elif event.button.id == "play":
-            if os.path.exists(path + "username"):
-                options["username"] = open(path + "username", "rt").read()
-            else:
-                with open(path + "username", "x") as unameFile:
-                    unameFile.write(options["username"])
-                    unameFile.close()
-            install.install_minecraft_version(minecraft_version, minecraft_directory)
-            minecraft_command = command.get_minecraft_command(minecraft_version, minecraft_directory, options)
-            if os.path.exists(path + "mc.log"):
-                os.remove(path + "mc.log")
-                logfile = open(path + "mc.log", "x")
-            else:
-                logfile = open(path + "mc.log", "x")
-            subprocess.run(minecraft_command, stdout=logfile)
-
-    def compose(self) -> ComposeResult:
-        yield Label("Version: " + str(minecraft_version))
-        yield Button("Play", id="play", variant="success")
-        yield Button("Settings", id="settings")
+if path.endswith("assets"):
+    path = "./"
+else:
+    path = "assets/"
 
 
-class SettingsPanel(Screen):
-
-    def on_button_pressed(self, event: Button.Pressed):
-        if event.button.id == "settings_back":
-            app.pop_screen()
-            app.push_screen("login")
-        elif event.button.id == "open-minecraft-dir":
-            webbrowser.open('file:///' + minecraft_directory)
-        elif event.button.id == "change-username":
-            subprocess.run([sys.executable, path + "changeUsername.py"])
-
-    def compose(self) -> ComposeResult:
-        yield Header()
-        yield Footer()
-        yield Button("Open .minecraft directory", id="open-minecraft-dir")
-        yield Button("Change username", id="change-username", variant="success")
-        yield Button("Back", variant="error", id="settings_back")
-
-
-class PyCrackMC(App):
-    BINDINGS = [("d", "toggle_dark", "Toggle Dark Mode"), ("q", "quit", "Quit")]
-    SCREENS = {
-        "login": LoginPanel(),
-        "settings": SettingsPanel()
-    }
-
-    def action_toggle_dark(self) -> None:
-        self.dark = not self.dark
-
-    def action_quit(self) -> None:
-        exit(0)
-
-    def compose(self) -> ComposeResult:
-        yield Header()
-        yield Footer()
-        app.push_screen("login")
-
-
-if __name__ == "__main__":
-    if path.endswith("assets"):
-        path = "./"
+def launch_minecraft():
+    if os.path.exists(path + "username"):
+        options["username"] = open(path + "username", "rt").read()
     else:
-        path = "assets/"
-    app = PyCrackMC()
-    app.run()
+        with open(path + "username", "x") as unameFile:
+            unameFile.write(options["username"])
+            unameFile.close()
+    install.install_minecraft_version(minecraft_version, minecraft_directory)
+    minecraft_command = command.get_minecraft_command(minecraft_version, minecraft_directory, options)
+    subprocess.run(minecraft_command)
+
+
+def openSettings():
+    subprocess.run([sys.executable, path + "settings.py"])
+
+
+root = tk.Tk()
+root.title("PyCraft Launcher")
+root.geometry("250x100")
+version_label = tk.Label(root, text="Version: " + minecraft_version)
+version_label.pack()
+play_button = tk.Button(root, text="Play", command=launch_minecraft)
+play_button.pack()
+settings_button = tk.Button(root, text="Settings", command=openSettings)
+settings_button.pack()
+
+root.mainloop()
